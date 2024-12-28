7f14594d9a99b70c554074fd916366682948900a
José Divino <josedivinooficial02@gmail.com>
2024-12-28 13:43:54 -0300

    Add guest middleware routes for login and registration

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['guest', 'web'])->group(function () {
+    Route::post('login', Auth\LoginController::class)->name('login');
+    Route::post('register', Auth\RegisterController::class)->name('register');
+});
