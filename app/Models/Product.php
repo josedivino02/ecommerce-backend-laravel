1da18e80969bfe7bdfbf44b5bdce19ec7b73b308
José Divino <josedivinooficial02@gmail.com>
2024-12-28 08:59:02 -0300

    Create Product Model

---
+++ app/Models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+}
