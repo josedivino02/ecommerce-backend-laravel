21a83b9db8321b6ba4e075058f18d84c8655a4db
José Divino <josedivinooficial02@gmail.com>
2024-12-28 13:42:46 -0300

    Create RegisterController

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function __invoke()
+    {
+        $data = request()->validate([
+            "name"     => ["required", "min:3", "max:255"],
+            "email"    => ["required", "min:3", "max:255", "email", "unique::users", "confirmed"],
+            "password" => ["required", "min:8", "max:40"],
+        ]);
+
+        $user = User::create($data);
+
+        auth()->login($user);
+    }
+}
