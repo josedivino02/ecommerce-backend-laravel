f3b3773369f72084e837693d49ddfd7d57ae772c
José Divino <josedivinooficial02@gmail.com>
2024-12-28 08:42:03 -0300

    Create StoreOrderRequest

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\AtLeastOneItem;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+/**
+ * @property-read string $order
+ */
+class StoreOrderRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return Gate::allows('create', $this->route()->order);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'shipping_address' => ['required', 'string'],
+            'billing_address'  => ['nullable', 'string'],
+            'payment_method'   => ['required', 'string'],
+            'shipping_method'  => ['required', 'string'],
+            'shipping_cost'    => ['required', 'numeric'],
+            'total_price'      => ['required', 'numeric'],
+            'discount'         => ['required', 'numeric'],
+            'items'            => ['required', 'array', new AtLeastOneItem()],
+        ];
+    }
+}
