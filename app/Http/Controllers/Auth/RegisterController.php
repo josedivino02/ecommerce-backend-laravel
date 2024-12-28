82c1101eff0b42fbcf24ebd24afd1108db472def
José Divino <josedivinooficial02@gmail.com>
2024-12-28 17:11:29 -0300

    Create RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,19 +4,30 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\{Request};
+use Illuminate\Support\Facades\{Hash, Validator};
+use Symfony\Component\HttpFoundation\Response;
 
 class RegisterController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $data = request()->validate([
+        $validator = Validator::make($request->all(), [
             "name"     => ["required", "min:3", "max:255"],
             "email"    => ["required", "min:3", "max:255", "email", "unique::users", "confirmed"],
-            "password" => ["required", "min:8", "max:40"],
+            "password" => ["required", "min:8", "max:40", "confirmed"],
         ]);
 
-        $user = User::create($data);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
-        auth()->login($user);
+        User::create([
+            "name"     => $request->name,
+            "email"    => $request->email,
+            "password" => Hash::make($request->password),
+        ]);
+
+        return response()->json(["message" => "User successfully registered"], Response::HTTP_CREATED);
     }
 }
