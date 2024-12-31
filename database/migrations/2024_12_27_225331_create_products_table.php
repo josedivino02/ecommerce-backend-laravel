420d589993191e571b59ae48a3db16048265a6bd
José Divino <josedivinooficial02@gmail.com>
2024-12-31 16:18:21 -0300

    adding the user relationship field to the product and status field

--- database/migrations/2024_12_27_225331_create_products_table.php
+++ database/migrations/2024_12_27_225331_create_products_table.php
@@ -12,12 +12,14 @@ return new class () extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->index();
             $table->string('name');
             $table->string('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->integer('stock');
             $table->string('sku')->unique();
             $table->string('image_url')->nullable();
+            $table->string("status")->default("active");
             $table->softDeletes();
             $table->timestamps();
         });
