5a96abc4e141543550fb7ca5e626eff0a56e09f9
José Divino <josedivinooficial02@gmail.com>
2024-12-31 16:33:16 -0300

    Refactoring the routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\Product;
 use App\Http\Controllers\{Auth, Order};
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [Auth\RegisterController::class, 'register'])->name("register");
 Route::post('/login', [Auth\LoginController::class, "login"])->name("login");
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', [Auth\LogoutController::class, "logout"])->name("logout");
     Route::post("/orders", [Order\StoreController::class, "store"])->name("orders.store");
+
+    Route::prefix("products")->middleware(IsAdminMiddleware::class)->group(function () {
+        Route::post("/", [Product\StoreController::class, "store"])->name("products.store");
+    });
+
+    Route::post('/logout', [Auth\LogoutController::class, "logout"])->name("logout");
 });
