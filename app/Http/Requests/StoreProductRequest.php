d45a3f16856b217832ab8ce3899ebbf2132fdc63
José Divino <josedivinooficial02@gmail.com>
2024-12-31 16:29:58 -0300

    Adds Store Product FormRequest

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -2,27 +2,44 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Product;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class StoreProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return Gate::allows("isAdmin", Product::class);
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            "name"        => ["required", "max:255"],
+            "description" => ["required"],
+            "price"       => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/", "min:0.01"],
+            "stock"       => ["required", "numeric", "integer", "min:0"],
+            "sku"         => ["required", "unique:products,sku", "max:50"],
+            "image_url"   => ["nullable", "url"],
         ];
     }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            "errors" => $validator->errors(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    public function messages(): array
+    {
+        return [
+            "sku.unique" => "The SKU entered is already registered in the system.",
+        ];
+    }
+
 }
