f22de5739a4c345c3d215b59de70c1c1e7b8ef74
José Divino <josedivinooficial02@gmail.com>
2025-01-04 00:07:55 -0300

    fix enums validate

--- app/Rules/ValidOrderForCancellation.php
+++ app/Rules/ValidOrderForCancellation.php
@@ -20,25 +20,25 @@ class ValidOrderForCancellation implements ValidationRule
             return;
         }
 
-        if ($order->status === OrderStatus::CANCELED) {
+        if ($order->status === OrderStatus::CANCELED->value) {
             $fail("The requested order has already been canceled.");
 
             return;
         }
 
-        if ($order->payment_status === PaymentStatus::COMPLETED) {
+        if ($order->payment_status === PaymentStatus::COMPLETED->value) {
             $fail("Payment for the requested order has already been made. To proceed with the cancellation, please request the corresponding refund.");
 
             return;
         }
 
-        if ($order->payment_status === ShippingStatus::SHIPPED) {
+        if ($order->payment_status === ShippingStatus::SHIPPED->value) {
             $fail("The requested order has already been sent to the registered address. To proceed with the cancellation, please request the corresponding refund.");
 
             return;
         }
 
-        if ($order->status === OrderStatus::COMPLETED) {
+        if ($order->status === OrderStatus::COMPLETED->value) {
             $fail("The requested order has already been completed and cannot be canceled.");
         }
     }
