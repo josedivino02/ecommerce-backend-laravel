5be58b9dec68ada3dfe8e0af0d5a9e5dfd4e8c7f
José Divino <josedivinooficial02@gmail.com>
2025-01-03 22:25:20 -0300

    Validated Order for cancellation rule

---
+++ app/Rules/ValidOrderForCancellation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Rules;
+
+use App\Enums\{OrderStatus, PaymentStatus, ShippingStatus};
+use App\Models\Order;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidOrderForCancellation implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $order = Order::where("uuid", $value)
+            ->first();
+
+        if (!$order) {
+            $fail("Requested order were not found for cancellation.");
+
+            return;
+        }
+
+        if ($order->status === OrderStatus::CANCELED) {
+            $fail("The requested order has already been canceled.");
+
+            return;
+        }
+
+        if ($order->payment_status === PaymentStatus::COMPLETED) {
+            $fail("Payment for the requested order has already been made. To proceed with the cancellation, please request the corresponding refund.");
+
+            return;
+        }
+
+        if ($order->payment_status === ShippingStatus::SHIPPED) {
+            $fail("The requested order has already been sent to the registered address. To proceed with the cancellation, please request the corresponding refund.");
+
+            return;
+        }
+
+        if ($order->status === OrderStatus::COMPLETED) {
+            $fail("The requested order has already been completed and cannot be canceled.");
+        }
+    }
+}
