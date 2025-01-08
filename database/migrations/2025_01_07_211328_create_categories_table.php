e75e6a9edc224a0d4df2e504a45bbbe97f9f7e76
José Divino <josedivinooficial02@gmail.com>
2025-01-07 21:55:08 -0300

    Adding the category_id field to the products table, establishing a foreign key relationship with the categories table.

--- database/migrations/2025_01_07_211328_create_categories_table.php
+++ database/migrations/2025_01_07_211328_create_categories_table.php
@@ -12,6 +12,7 @@ return new class () extends Migration {
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string("name");
             $table->string("slug");
             $table->integer("sub")->nullable();
