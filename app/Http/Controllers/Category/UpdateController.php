9f8262d611866041a75e37bebe218753836a7bbd
José Divino <josedivinooficial02@gmail.com>
2025-01-14 19:45:43 -0300

    Fixed the way data is saved when updating a category

--- app/Http/Controllers/Category/UpdateController.php
+++ app/Http/Controllers/Category/UpdateController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers\Category;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class UpdateController extends Controller
 {
     public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //Alterar isso aqui, para gravar de forma correta
-        $category->name        = $request->name;
-        $category->description = $request->has('description') ?? $request->description;
-        $category->status      = $request->has('status') ?? $request->status;
-        $category->save();
+        $category->update(array_merge(
+            $request->only(["name", "description", "status"]),
+            ["slug" => Str::slug($request->name)]
+        ));
 
         return response()->json([
             "success" => "The Category successfully updated",
