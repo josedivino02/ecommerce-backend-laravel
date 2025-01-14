2e7807b71aea473ce710a7e45135f62b4e95c573
José Divino <josedivinooficial02@gmail.com>
2025-01-13 21:30:16 -0300

    Adds route category list

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware([JwtAuthenticationMiddleware::class])->group(function () {
 
     Route::prefix("categories")->middleware(IsAdminMiddleware::class)->group(function () {
         Route::post("/", [Category\StoreController::class, "store"])->name("categories.store");
+        Route::get("/", [Category\IndexController::class, "index"])->name("categories.index");
     });
 
     Route::post('/logout', [Auth\LogoutController::class, "logout"])->name("logout");
