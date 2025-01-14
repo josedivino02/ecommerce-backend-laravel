b8dfd1e395a45d02dedf8f8601771f6cd3cd83fe
José Divino <josedivinooficial02@gmail.com>
2025-01-14 00:21:06 -0300

    Adds ValidCategoryStatus

---
+++ app/Rules/ValidCategoryStatus.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Rules;
+
+use App\Enums\CategoryStatus;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidCategoryStatus implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!in_array($value, CategoryStatus::values())) {
+            $fail("The $attribute field must be a valid category status.");
+        }
+    }
+}
