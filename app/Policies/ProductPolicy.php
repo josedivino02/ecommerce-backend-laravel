6fa9810edfb914519a344358539d5c634d0e7ae5
José Divino <josedivinooficial02@gmail.com>
2025-01-17 23:22:08 -0300

    Adding a product update policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -6,8 +6,13 @@ use App\Models\User;
 
 class ProductPolicy
 {
-    public function isAdmin(User $user): bool
+    public function isAdmin(): bool
     {
         return user()->isAdmin === 'S';
+    }
+
+    public function update(User $user)
+    {
+        return auth()->check() && $this->isAdmin();
     }
 }
