8aa9c2a224762527b36cbc2b9ff0a643237b8af0
José Divino <josedivinooficial02@gmail.com>
2025-01-23 22:51:28 -0300

    Implements Repository pattern and Service Layer to the Category module: delete

--- app/Http/Controllers/Category/DeleteController.php
+++ app/Http/Controllers/Category/DeleteController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Category;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DeleteCategoryRequest;
 use App\Models\Category;
+use App\Services\Category\CategoryService;
 
 class DeleteController extends Controller
 {
+    public function __construct(private CategoryService $categoryService)
+    {
+    }
+
     public function delete(DeleteCategoryRequest $request, Category $category)
     {
-        $category->delete();
+        $this->categoryService->delete($category);
 
         return response()->noContent();
     }
--- app/Services/Category/CategoryService.php
+++ app/Services/Category/CategoryService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Category;
 
+use App\Enums\CategoryStatus;
 use App\Models\Category;
 use App\Repositories\Category\CategoryRepositoryInterface;
 use Illuminate\Support\Str;
@@ -14,7 +15,14 @@ class CategoryService
 
     public function create(array $data): Category
     {
-        $data['slug'] = Str::slug($data['name']);
+        $data["uuid"]   = Str::uuid();
+        $data["slug"]   = Str::slug($data["name"]);
+        $data["status"] = CategoryStatus::ACTIVE;
+
+        if (isset($data["subcategory"])) {
+            $data['sub'] = $data['subcategory'];
+            unset($data['subcategory']);
+        }
 
         return $this->categoryRepository
             ->create($data);
