8d9cbff24dee62b1385009af846337630d97e3aa
José Divino <josedivinooficial02@gmail.com>
2025-01-25 21:11:13 -0300

    Implements Service create Order

---
+++ app/Services/Order/OrderService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services\Order;
+
+use App\Enums\{OrderItemsStatus, OrderStatus, PaymentStatus, ShippingStatus};
+use App\Models\Order;
+use App\Repositories\Order\OrderRepositoryInterface;
+
+use Illuminate\Support\Str;
+
+class OrderService
+{
+    public function __construct(protected OrderRepositoryInterface $orderRepository)
+    {
+    }
+
+    public function create(array $data): Order
+    {
+        $totalPrice = $this->calculateTotalPrice(
+            $data["items"],
+            $data["discount"]
+        );
+
+        $data["uuid"]              = Str::uuid();
+        $data["payment_status"]    = PaymentStatus::PENDING;
+        $data["shipping_status"]   = ShippingStatus::PROCESSING;
+        $data["verification_code"] = strtoupper(Str::random(10));
+        $data["status"]            = OrderStatus::PENDING;
+        $data["total_price"]       = $totalPrice;
+
+        $items = $data["items"];
+        unset($data["items"]);
+
+        $order = $this->orderRepository
+            ->create($data);
+
+        $this->addItems(
+            $order,
+            $items
+        );
+
+        return $order;
+    }
+
+    private function addItems(Order $order, array $items): void
+    {
+        collect($items)->each(function ($item) use ($order) {
+            $item["uuid"]        = Str::uuid();
+            $item["total_price"] = $item["unit_price"] * $item["quantity"];
+            $item["tracking"]    = now()->timestamp . rand(100, 999);
+            $item["status"]      = OrderItemsStatus::PENDING;
+
+            $this->orderRepository->addItems($order, $item);
+        });
+    }
+
+    private function calculateTotalPrice(array $items, float $discount): float
+    {
+        $totalPrice = collect($items)->reduce(
+            function ($acc, $item) {
+                return $acc + ($item['unit_price'] * $item['quantity']);
+            },
+            0
+        );
+
+        return $totalPrice - $discount;
+    }
+}
