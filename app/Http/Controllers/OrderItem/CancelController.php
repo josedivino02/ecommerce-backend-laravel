2c39b4907df28baf811233af363430f19de24d20
José Divino <josedivinooficial02@gmail.com>
2025-01-26 00:08:43 -0300

    Bind Service Cancel item with Controller Cancel Item

--- app/Http/Controllers/OrderItem/CancelController.php
+++ app/Http/Controllers/OrderItem/CancelController.php
@@ -2,31 +2,31 @@
 
 namespace App\Http\Controllers\OrderItem;
 
-use App\Enums\OrderItemsStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CancelOrderItemRequest;
 use App\Models\{Order, OrderItem};
+use App\Services\Item\CancelItemService;
 use Symfony\Component\HttpFoundation\Response;
 
 class CancelController extends Controller
 {
+    public function __construct(protected CancelItemService $itemService)
+    {
+    }
+
     public function cancel(CancelOrderItemRequest $request, Order $order, OrderItem $item)
     {
         try {
-            $order->update([
-                "total_price" => ($order->total_price - $item->total_price),
-            ]);
+            $canceledItem = $this->itemService->cancel($order, $item);
 
-            $item->update([
-                "status" => OrderItemsStatus::CANCELED,
-            ]);
-
-            return response()->json(
-                [
-                    "success" => "The item related to the order have been canceled",
-                ],
-                Response::HTTP_OK
-            );
+            if ($canceledItem) {
+                return response()->json(
+                    [
+                        "success" => "The item related to the order have been canceled",
+                    ],
+                    Response::HTTP_OK
+                );
+            }
         } catch (\Exception $e) {
             return response()->json(["error" => "Unexpected error"], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
