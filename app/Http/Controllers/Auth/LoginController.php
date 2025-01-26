517279e041ed7093a443c24fb3b34af7c0e474df
José Divino <josedivinooficial02@gmail.com>
2025-01-26 20:40:23 -0300

    Implements Auth Service in Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,41 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Services\Auth\AuthService;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function __construct(protected AuthService $authService)
     {
-        $request->validate([
-            "email"    => ["required", "email"],
-            "password" => ["required"],
-        ]);
+    }
 
+    public function login(LoginRequest $request)
+    {
         $credentials = $request->only("email", "password");
 
         try {
-            if (!$token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'Invalid credentials!'], Response::HTTP_UNAUTHORIZED);
-            }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'It was not possible to create the token.'], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+            $response = $this->authService->login($credentials);
 
-        return response()->json(
-            [
-                'access_token' => $token,
-                'token_type'   => "bearer",
-                "expires_in"   => config('jwt.ttl') * 60,
-            ],
-            Response::HTTP_OK
-        );
+            if (!$response) {
+                return response()->json(
+                    ['error' => 'Invalid credentials!'],
+                    Response::HTTP_UNAUTHORIZED
+                );
+            }
+
+            return response()->json(
+                $response,
+                Response::HTTP_OK
+            );
+
+        } catch (JWTException $e) {
+            return response()->json(
+                ['error' => 'It was not possible to create the token.'],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
